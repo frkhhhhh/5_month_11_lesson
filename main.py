42652def9be48bd443748b833fe575055fd21f43
frkhhhhh <164884541+frkhhhhh@users.noreply.github.com>
2024-04-20 16:03:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
-# import asyncio
-# import httpx
 
 
-# urls=[
-#     "https://jsonplaceholder.typicode.com/users/1",
-#     "https://jsonplaceholder.typicode.com/users/2",
-#     "https://jsonplaceholder.typicode.com/users/3",
-#     "https://jsonplaceholder.typicode.com/users/4",
-#     "https://jsonplaceholder.typicode.com/users/5"
-# ]
-
-# async def fetch_data(url):
-#     async with httpx.AsyncClient() as client:
-#         rsp= await client.get(url)
-#         return rsp.json()
-    
-# async def main():
-#     tm=[fetch_data(url) for url in urls]
-#     res=await asyncio.gather(*tm)
-#     for ress in res:
-#         print(ress)
-
-
-# asyncio.run(main())   
+ 
 import asyncio
 import httpx
 urls=[
